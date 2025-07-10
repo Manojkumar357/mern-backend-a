91c965c6a3712fd8c91a4e754c32853b2627dede
Manojkumar357 <akulamanojkumar357@gmail.com>
2025-07-10 11:17:16 +0530

    update

--- server.js
+++ server.js
@@ -2,19 +2,24 @@ const mongoose=require('mongoose');
 const express = require('express');
 const dotenv =require( 'dotenv');
 const userRouter =require( "./routes/userRoute.js");
+const cors=require('cors');
 dotenv.config();
 
 const app = express();
+app.use(cors())
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
 
+
 // mongoose
-//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .connect(`mongodb://localhost:27017/merncafe`)
 //   .then(() => {
 //     app.listen(8080, () => {
 //       console.log("Server started");
 //     });
 //   });
+
+
   mongoose
   .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.gqef1m3.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
